c57f7f7311b28b02d20821e984ca4124b14a334b
vagrant <vagrant@homestead>
2020-10-20 09:07:00 +0000

    321

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('stock:get')->daily();
+        $schedule->command('stock:get')->daily()->withoutOverlapping();
     }
 
     /**
